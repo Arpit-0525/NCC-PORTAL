ee79fc7308f16232f28eeba195251d0c30559bb0
Arpit-0525 <sarpit0203@gmail.com>
2026-05-06 21:51:34 +0530

    minor fixes

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex">
+    <div className="flex bg-gray-50 min-h-screen">
       <Sidebar />
 
       <div className="flex-1 flex flex-col">
